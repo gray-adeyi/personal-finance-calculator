81d1cdc59373cb6c62872e2d920c41ee166cb80a
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-01-11 18:11:11 +0100

    Implement backend logic

--- app.go
+++ app.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
+
+const TaxRate = 0.3
+const OwnerRate = 0.55
+const ProfitRate = 0.05
+const OpExRate = 0.1
+
+type FinanceResult struct {
+	Taxes  float32 `json:"taxes"`
+	Owner  float32 `json:"owner"`
+	Profit float32 `json:"profit"`
+	OpEx   float32 `json:"opEx"`
+}
 
 // App struct
 type App struct {
@@ -21,7 +35,27 @@ func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
 }
 
-// Greet returns a greeting for the given name
-func (a *App) Greet(name string) string {
-	return fmt.Sprintf("Hello %s, It's show time!", name)
+// CalculatePersonalFinance calculates and returns the finance result to the frontend.
+//
+// This is a simple calculation that can be executed in the frontend, but this is just a toy
+// project that shows how communications between the frontend and backend happens in Wails
+func (a *App) CalculatePersonalFinance(income float32) (*FinanceResult, error) {
+	if income <= 0.0 {
+		return nil, errors.New(fmt.Sprintf("The provided income of %.2f is invalid", income))
+	}
+	return &FinanceResult{
+		Taxes:  TaxRate * income,
+		Owner:  OwnerRate * income,
+		Profit: ProfitRate * income,
+		OpEx:   OpExRate * income,
+	}, nil
+}
+
+func (a *App) ShowErrorDialog(message string) error {
+	_, err := runtime.MessageDialog(a.ctx, runtime.MessageDialogOptions{
+		Type:    runtime.ErrorDialog,
+		Title:   "Error",
+		Message: message,
+	})
+	return err
 }
