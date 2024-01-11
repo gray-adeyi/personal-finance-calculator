474485c474a5aef0e031dbaf03ba82c501a35f09
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-01-11 18:13:53 +0100

    Implement app frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,51 @@
 <script lang="ts" setup>
-import HelloWorld from './components/HelloWorld.vue'</script>
+import AppInput from "./components/AppInput.vue";
+import AppButton from "./components/AppButton.vue";
+import ResultCard from "./components/ResultCard.vue";
+import {ref} from 'vue';
+import {calculatePersonalFinance} from "./backendService";
+import {main} from "../wailsjs/go/models";
+import {ShowErrorDialog} from "../wailsjs/go/main/App";
+
+const _income = ref(0)
+const _result = ref<main.FinanceResult|null>(null)
+
+async function calculate(){
+  try {
+    _result.value = await calculatePersonalFinance(_income.value)
+  } catch (e) {
+    await ShowErrorDialog((e as Error).toString())
+  }
+}
+</script>
 
 <template>
-  <img id="logo" alt="Wails logo" src="./assets/images/logo-universal.png"/>
-  <HelloWorld/>
+  <div class="app">
+    <h1 class="app__header">Enter Total Income</h1>
+    <AppInput class="app__input" v-model.number="_income" @keyup.enter="calculate" />
+    <AppButton class="app__button" @click="calculate" />
+    <ResultCard class="app__result-card" :result="_result" />
+  </div>
 </template>
 
-<style>
-#logo {
-  display: block;
-  width: 50%;
-  height: 50%;
-  margin: auto;
-  padding: 10% 0 0;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: 100% 100%;
-  background-origin: content-box;
+<style scoped>
+.app{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 2rem;
+
+  .app__header{
+    margin-block-start: 3rem;
+    margin-block-end: 2rem;
+  }
+
+  .app__input{
+    margin-block-end: 2rem;
+  }
+
+  .app__button{
+    margin-block-end: 3rem;
+  }
 }
 </style>
