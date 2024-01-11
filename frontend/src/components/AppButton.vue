05853bca78892107bf27d60a630fd16e6ed40f33
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-01-11 18:10:00 +0100

    Implement AppButton component

---
+++ frontend/src/components/AppButton.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup></script>
+
+<template>
+  <button>Calculate</button>
+</template>
+
+<style scoped>
+button{
+  display: block;
+  width: 90%;
+  text-align: center;
+  padding: .5rem;
+  color: white;
+  background: var(--app-accent-color);
+  border: none;
+  border-radius: 7px;
+  font-weight: bold;
+  font-size: 1rem;
+  cursor: pointer;
+  transition: background 500ms;
+
+  &:hover{
+    background: #598bf6;
+  }
+
+  &:active{
+    background: var(--app-accent-color);
+  }
+}
+</style>
