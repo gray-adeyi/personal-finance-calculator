b87e8ebf31f5d52b394504b1ea6f87bd7ccda301
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-01-11 18:09:34 +0100

    Implement AppInput component

---
+++ frontend/src/components/AppInput.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+const model = defineModel({required: true, default: 0})
+</script>
+
+<template>
+  <input type="text" v-model.number="model" />
+</template>
+
+<style scoped>
+input {
+  display: block;
+  width: 100%;
+  outline: none;
+  border: 2px solid var(--app-accent-color);
+  background: #5d5d5dff;
+  padding: .5rem;
+  text-align: center;
+  color: white;
+}
+</style>
