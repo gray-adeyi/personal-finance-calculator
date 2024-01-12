a1aee4e99ab4036730d8954fbfc8b1b32e6f973c
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-01-12 03:55:40 +0100

    Fix indexed languages

---
+++ frontend/wailsjs/go/models.ts
@@ -0,0 +1,23 @@
+export namespace main {
+	
+	export class FinanceResult {
+	    taxes: number;
+	    owner: number;
+	    profit: number;
+	    opEx: number;
+	
+	    static createFrom(source: any = {}) {
+	        return new FinanceResult(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.taxes = source["taxes"];
+	        this.owner = source["owner"];
+	        this.profit = source["profit"];
+	        this.opEx = source["opEx"];
+	    }
+	}
+
+}
+
