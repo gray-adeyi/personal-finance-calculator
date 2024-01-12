4497a553006df5b48ed970940715b521031546b0
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-01-12 03:13:39 +0100

    Resolve conflicts

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -21,3 +21,27 @@ export namespace main {
 
 }
 
+
+export namespace main {
+	
+	export class FinanceResult {
+	    taxes: number;
+	    owner: number;
+	    profit: number;
+	    opEx: number;
+	
+	    static createFrom(source: any = {}) {
+	        return new FinanceResult(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.taxes = source["taxes"];
+	        this.owner = source["owner"];
+	        this.profit = source["profit"];
+	        this.opEx = source["opEx"];
+	    }
+	}
+
+}
+
